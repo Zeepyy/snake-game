1495fd4d2e10780e81615a91d9e8fe2b493949a1
zey <zeynepuyanmis@gmail.com>
2024-05-18 13:37:28 +0300

    Add project files.

---
+++ yilanoyunu/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace yilanoyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.game = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.game)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // game
+            // 
+            this.game.AccessibleName = "";
+            this.game.BackColor = System.Drawing.Color.White;
+            this.game.Location = new System.Drawing.Point(12, 12);
+            this.game.Name = "game";
+            this.game.Size = new System.Drawing.Size(500, 500);
+            this.game.TabIndex = 0;
+            this.game.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(444, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Total:99";
+           
+            // 
+            // Form1
+            // 
+            this.AccessibleName = "";
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(616, 554);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.game);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.game)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox game;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ yilanoyunu/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace yilanoyunu
+{
+    public partial class Form1 : Form
+    {
+
+        public enum Direction
+        {
+            Up,
+            Down,
+            Left,
+            Right
+        }
+        public int score = 0;
+        public int X = 1, Y = 1; 
+        public Direction dr=Direction.Right;
+        public Location feed = new Location(-1, -1);
+        public List<Location> tales=new List<Location>();
+        public bool Game = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            tales.Add(new yilanoyunu.Location(0, 0));
+
+            CalcTable();
+
+            Thread thread = new Thread(new ThreadStart(new Action(() =>
+            {
+                while (Game)
+                {
+                    if (dr == Direction.Right) X++;
+                    if (dr==Direction.Down) Y++;
+                    if(dr==Direction.Up) Y--;
+                    if(dr==Direction.Left) X--;
+                    CalcTable();
+                    Thread.Sleep(100);
+                }
+               
+            })));
+            thread.Start();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode.ToString() == "S") dr = Direction.Down;
+            else if (e.KeyCode.ToString() == "A") dr = Direction.Left;
+            else if (e.KeyCode.ToString() == "D") dr = Direction.Right;
+            else if (e.KeyCode.ToString() == "W") dr = Direction.Up;
+
+        }
+
+        
+
+        public void CalcTable()
+        {
+            try { Invoke(new Action(() => label1.Text = "Total:" + score)); }
+            catch { }
+
+            Random rnd = new Random();
+            Bitmap bitmap = new Bitmap(500, 500);
+
+
+            //tales coll
+            if (tales.Count != 1)
+            {
+                for (int i = 1; i < tales.Count; i++)
+                {
+                    if (tales[i].x == X && tales[i].y == Y)
+                    {
+                        Game = false;   
+                        MessageBox.Show("Kaybettin!");
+                        
+                    }
+                }
+            }
+
+
+            //feed coll
+            if (X==feed.x && Y==feed.y)
+            {
+                score++;
+                tales.Add(new yilanoyunu.Location(feed.x, feed.y));
+                feed = new Location(-1, -1);
+
+            }
+
+            //outside coll
+            if(X<=0 || Y<=0 || X == 51 || Y == 51)
+            {
+
+                Game=false;
+                MessageBox.Show("Kaybettin!");
+               
+            }
+            else
+            {
+                //snake
+                for (int i = (X - 1) * 10; i < X * 10; i++)
+                {
+                    for (int j = (Y - 1) * 10; j < Y * 10; j++)
+                    {
+                        bitmap.SetPixel(i, j, Color.Black);
+                    }
+                }
+            }
+       
+
+
+
+
+            //tales
+            if (tales.Count != 1)
+            {
+                for (int k = 0; k < tales.Count; k++)
+                {
+                    for (int i = (tales[k].x - 1) * 10; i < tales[k].x * 10; i++)
+                    {
+                        for (int j = (tales[k].y - 1) * 10; j < tales[k].y * 10; j++)
+                        {
+                            bitmap.SetPixel(i, j, Color.Black);
+                        }
+                    }
+                }
+            }
+
+            tales[0] = new Location(X, Y);
+            for(int i =tales.Count - 1; i > 0; i--)
+            {
+                tales[i] = tales[i - 1];
+            }
+
+         
+
+
+            //feed
+            if (feed.x==-1)
+            {
+                feed= new Location(rnd.Next(1, 50), rnd.Next(1, 50));
+            }
+
+            for (int i = (feed.x - 1) * 10; i < feed.x * 10; i++)
+            {
+                for (int j = (feed.y - 1) * 10; j < feed.y * 10; j++)
+                {
+                    bitmap.SetPixel(i, j, Color.Red);
+                }
+            }
+
+            game.Image= bitmap;
+        }
+
+    }
+
+    public class Location
+    {
+        public int x, y;
+        public Location(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+}
